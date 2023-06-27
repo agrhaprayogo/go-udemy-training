f175aeb07da4d1917003071125dd8511b69757f1
agrha prayogo <agrha_a152@mitrais.com>
2023-06-27 12:30:54 +0700

    multiple return values

--- main.go
+++ main.go
@@ -1,2 +1,12 @@
 package main
 
+func main() {
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Diamonds")
+
+	cards.print()
+}
+
+func newCard() string {
+	return "Five Of Diamonds"
+}
